0c79ed02c4a230189752cd1282c6aaf02770c6b4
Prashakthi <webchamzz@gmail.com>
2024-05-03 15:28:41 +1000

    enable any types in APi call

--- pages/api/topTokens.ts
+++ pages/api/topTokens.ts
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+export default async function handler(req: any, res: any) {
   try {
     const response = await fetch(
       "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=25&sort=market_cap&convert=USD",
