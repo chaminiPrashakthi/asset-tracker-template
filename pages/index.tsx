c3668a0b0ae1af78566c72a6e940c94e017422bd
Prashakthi <webchamzz@gmail.com>
2024-05-03 15:10:53 +1000

    fix import error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import { Card, Flex, Heading, ListItem, Text, UnorderedList } from "@chakra-ui/react";
 import { NextPage } from "next";
-import Header from "../components/Header";
 import TokenList from "../components/tokenList";
 
 const Home: NextPage = () => (
